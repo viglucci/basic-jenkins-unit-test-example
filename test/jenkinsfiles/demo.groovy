a9ccc8087a9c5a5b2ba59c8324592699d55fd348
Kevin Viglucci <kviglucci@gmail.com>
2023-04-22 22:50:53 -0500

    feat: basic unit test

---
+++ test/jenkinsfiles/demo.groovy
@@ -0,0 +1,15 @@
+/**
+ * This is a real Jenkinsfile under test.
+ */
+
+node() {
+  stage('Checkout') {
+    // checkout scm
+    // sh 'git clean -xdf'
+  }
+
+  stage('Build and test') {
+    // sh './gradlew build'
+    // junit 'build/test-results/test/*.xml'
+  }
+}
